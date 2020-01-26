432aa304576a397f7f3ad82248eeb6f7bc618af2
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-01-26 11:39:10 +0530

    Add files via upload

---
+++ Find duplicates in O(n) time and O(1) extra space .cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+int main()
+{
+ hs;
+ ll t;
+ cin>>t;
+ f(t)
+ {
+  ll n;
+  cin>>n;
+  ll a[n];
+  f(n)
+   cin>>a[i];
+  ll ff=0;
+  for(ll i=0;i<n;i++)
+  {
+   if(a[abs(a[i])]>=0)
+     a[abs(a[i])]=-a[abs(a[i])];
+   else
+   {
+    cout<<abs(a[i])<<" ";
+    ff=1;
+   }
+
+  }
+  if(ff==0)
+    cout<<"-1";
+  cout<<"\n";
+ }
+ return 0;
+}
