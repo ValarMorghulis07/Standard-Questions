31ee5dbaef8be75bb33f4102c17e79f25c0db197
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-23 12:57:31 +0530

    Update Quick Left and right Rotation.cpp

--- Quick Left and right Rotation.cpp
+++ Quick Left and right Rotation.cpp
@@ -52,3 +52,39 @@ int main()
  return 0;
 
 }
+
+
+// clockwise
+
+class Solution {
+public:
+    void doit(vector<int>& nums,int start,int end)//O(n) && o(1)
+    {
+     while(start<end)
+     {
+      int temp=nums[start];
+      nums[start]=nums[end];
+      nums[end]=temp;
+      start++;
+      end--;
+     }
+    }
+    void rotate(vector<int>& nums, int k)
+    {
+     int n=nums.size();
+     if(n==0)
+         return;
+     k%=n;
+     /*int pp=n-k; if(anticlockwise then pp=k,&& rest will be same) O(n) && O(n)
+     vector<int>vv;
+     for(int i=pp;i<pp+n;i++)
+        vv.push_back(nums[i%n]);
+     for(int i=0;i<n;i++)
+         nums[i]=vv[i];
+     */
+        
+     doit(nums,0,n-1);
+     doit(nums,0,k-1);
+     doit(nums,k,n-1);
+    }
+};
