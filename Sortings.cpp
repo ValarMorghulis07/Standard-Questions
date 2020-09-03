7453b7f8f91a57372c819e583e4c531753e23271
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-03 19:00:15 +0530

    Update Sortings.cpp

--- Sortings.cpp
+++ Sortings.cpp
@@ -39,3 +39,32 @@ void mergsort(int arr[],int l,int r)
   merge(arr,l,mid,r);
  }
 }
+
+// Quick Sort
+
+int partition (int arr[], int low, int high)
+{
+ int pivot=arr[high];
+ int i=low-1;
+ for(int j=low;j<high;j++)
+ {
+  if(arr[j]<pivot)
+  {
+   i++;
+   swap(arr[i],arr[j]);
+  }
+ }
+ swap(arr[i+1],arr[high]);
+ return i+1;
+}
+
+void quicksort(int arr[],int low,int high)
+{
+ if(low<high)
+ {
+  int pi=partition(arr,low,high);
+  quicksort(arr,low,pi-1);
+  quicksort(arr,pi+1,high);
+ }
+}
+
