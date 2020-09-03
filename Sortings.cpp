e44d2e8be4abad5f271d576cfbc8fdae91a9f31a
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-03 19:29:10 +0530

    Update Sortings.cpp

--- Sortings.cpp
+++ Sortings.cpp
@@ -83,3 +83,17 @@ void selectionSort(int arr[], int n)
     swap(arr[i],arr[idx]);
    }
 }
+
+// Bubble Sort(Repeatedly swapping adjacent elements,sending max element at last in consecutive loops)
+
+void bubble(int arr[], int i, int n)
+{
+ for(int i=0;i<n-1;i++)
+ {
+ for(int j=0;j<n-i-1;j++)
+ {
+  if(arr[j]>arr[j+1])
+   swap(arr[j],arr[j+1]);
+ }
+ }
+}
