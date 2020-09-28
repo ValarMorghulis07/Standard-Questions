0ac9c544ce78de12e92d6defe25fcd5dc4de3fde
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-29 04:08:13 +0530

    Update Search element in 2D matrix .cpp

--- Search element in 2D matrix .cpp
+++ Search element in 2D matrix .cpp
@@ -1,123 +1,38 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+// 240. Search a 2D Matrix II
 
-const ll N=1e3+7;
-ll a[N][N];
-ll m,n;
+/*
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+*/
 
-/*void BinarySearch(ll p,ll j_low,ll j_high,ll x)
-{
- while(j_low<=j_high)
- {
-  ll mid=(j_low+j_high)/2;
-  if(a[p][mid]==x)
-  {
-   cout<<"found at"<<" "<<p<<" "<<mid<<endl;
-   return;
-  }
-  else if(a[p][mid]>x)
-    j_high=mid-1;
-  else
-    j_low=mid+1;
- }
- cout<<"Element Not found"<<endl;
-}
-
-void BinaryUtil1(ll x)
-{
- if(m==1)
- {
-  BinarySearch(0,0,n-1,x);
-  return;
- }
- ll i_low=0,i_high=m-1,j_mid=n/2;
- while((i_low+1)<i_high)
- {
-  ll i_mid=(i_low+i_high)/2;
-  if(a[i_mid][j_mid]==x)
-  {
-   cout<<"found at"<<" "<<i_mid<<" "<<j_mid<<endl;
-   return;
-  }
-  else if(a[i_mid][j_mid]>x)
-    i_high=i_mid;
-  else
-    i_low=i_mid;
- }
- if(a[i_low][j_mid]==x)
-  cout<<"found at"<<" "<<i_low<<" "<<j_mid<<endl;
- else if(a[i_high][j_mid]==x)
-  cout<<"found at"<<" "<<i_high<<" "<<j_mid<<endl;
- else if(x<a[i_low][j_mid])
-    BinarySearch(i_low,0,j_mid-1,x);
- else if(x>a[i_low][j_mid] && x<=a[i_low][n-1])
-    BinarySearch(i_low,j_mid+1,n-1,x);
- else if(x<a[i_high][j_mid])
-    BinarySearch(i_high,0,j_mid-1,x);
- else
-    BinarySearch(i_high,j_mid+1,n-1,x);
-}*/
-
-void BinaryUtil2(ll x)
-{
- ll smallest=a[0][0],largest=a[m-1][n-1];
- if(x<smallest || x>largest)
- {
-  cout<<"Element Not found"<<endl;
-  return;
- }
- ll i=0,j=n-1;
- while(i<n && j>=0)
- {
-  if(a[i][j]==x)
-  {
-   cout<<"found at"<<" "<<i<<" "<<j<<endl;
-   return;
-  }
-  else if(a[i][j]>x)
-    j--;
-  else
-    i++;
- }
- cout<<"Element Not found"<<endl;
-}
-
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  cin>>m>>n;
-  f(m)
-  {
-   r(n)
-   {
-    cin>>a[i][j];
-   }
-  }
-  for(ll p=0;p<m*n;p++)
-  {
-  ll x;
-  cin>>x;
- // BinaryUtil1(x);//O(log n + log m);
-  BinaryUtil2(x);//O(m+n);
-  }
- }
- return 0;
-}
+class Solution {
+public:
+    bool bs(vector<vector<int>>& matrix,int x) // O(m+n)
+    {
+     int m=matrix.size();
+     int n=matrix[0].size();
+     if(x<matrix[0][0] || x>matrix[m-1][n-1])
+         return 0;
+     int i=0,j=n-1;
+     while(i<m && j>=0)
+     {
+      if(matrix[i][j]==x)
+          return true;
+      else if(matrix[i][j]>x)
+          j--;
+      else
+          i++;
+     }
+     return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+     int m=matrix.size();
+     if(m==0)
+         return 0;
+     int n=matrix[0].size();
+     if(n==0)
+         return 0;
+     return bs(matrix,target);
+    }
+};
