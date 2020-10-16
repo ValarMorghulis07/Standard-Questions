6e8989453f92ac0c4da331c391cbb82a9831758b
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-16 14:44:09 +0530

    Update Remove duplicates from a given string.cpp

--- Remove duplicates from a given string.cpp
+++ Remove duplicates from a given string.cpp
@@ -1,53 +1,6 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+// 1st method--> use map o(n) time and o(n) space
 
-char *checkduplicate(char str[],ll n)
-{
- ll index=0;
-
- for(ll i=0;i<n;i++)
- {
-  ll p;
-  for(p=0;p<i;p++)
-   if(str[p]==str[i])
-     break;
-
-  if(p==i)
-   str[index++]=str[i];
- }
- return str;
-}
-
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  char str[]="geeksforgeeks";
-  ll n=sizeof(str)/sizeof(str[0]);
-  cout<<checkduplicate(str,n)<<endl;
-
- }
- return 0;
-}
-
-// Remove duplicates from a string in O(1) extra space and o(n)
+//2nd---> Remove duplicates from a string in O(1) extra space and o(n)
 
 #include <bits/stdc++.h>
 #define mem(dp,a) memset(dp,a,sizeof(dp))
@@ -56,7 +9,7 @@ int main()
 #define rep(i,a,b) for(ll i=a;i<b;i++)
 #define repush_back(i,a,b) for(ll i=a;i>=b;i--)
 #define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
+#define r(n)>for(ll j=0;j<n;j++)
 #define F first
 #define S second
 #define pi 3.14159265359
