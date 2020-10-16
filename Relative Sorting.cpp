bdfc0c4a4626775a4f84ffe018361d0f243eb888
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-16 14:46:24 +0530

    Update Relative Sorting.cpp

--- Relative Sorting.cpp
+++ Relative Sorting.cpp
@@ -1,3 +1,12 @@
+/*
+Given two arrays A1[] and A2[], sort A1 in such a way that the relative order among the elements will be same as those are in A2. For the elements not present in A2, append them at last in sorted order. 
+Example: 
+
+Input: A1[] = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8}
+       A2[] = {2, 1, 8, 3}
+Output: A1[] = {2, 2, 1, 1, 8, 8, 3, 5, 6, 7, 9}
+*/
+
 #include <bits/stdc++.h>
 #define mem(dp,a) memset(dp,a,sizeof(dp))
 #define pb(x) push_back(x)
