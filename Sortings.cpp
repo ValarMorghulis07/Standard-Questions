0a19b25c3569758dd9446268daf226905133352e
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-28 23:15:12 +0530

    Update Sortings.cpp

--- Sortings.cpp
+++ Sortings.cpp
@@ -62,6 +62,15 @@ void mergsort(int arr[],int l,int r)
 
 // Quick Sort
 
+/*
+Is QuickSort stable?
+The default implementation is not stable. However any sorting algorithm can be made stable by considering indexes as comparison parameter.
+Is QuickSort In-place?
+
+As per the broad definition of in-place algorithm it qualifies as an in-place sorting algorithm as it uses extra space only for storing recursive function calls but 
+not for manipulating the input.
+*/
+
 int partition (int arr[], int low, int high)
 {
  int pivot=arr[high];
